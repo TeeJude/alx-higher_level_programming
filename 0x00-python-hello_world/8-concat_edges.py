42084e6ebaad31ef8718f357112dc2822c9d1886
Jude Tijani <teejayjude@gmail.com>
2023-06-06 11:50:55 -0700

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-orinted programming\
     language that combines remarkable power with very clear sytax"
-str = str[39:66] + str[109:114] + str[0:6]
+str = str[39:66] + str[109:114] + str[:6]
 print(str)
