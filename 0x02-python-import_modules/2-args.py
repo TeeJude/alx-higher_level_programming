3d44fe9c6bf370661b30a02910b05b5e369926f4
Jude Tijani <teejayjude@gmail.com>
2023-06-08 15:26:21 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    arg = sys.argv
+    count = len(arg) - 1
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count > 1:
+        print("{} arguments:".format(count))
+        for i in range(1, count + 1):
+            print("{}: {}".format(i, arg[i]))
+    else:
+        print("{} argument:".format(count))
+        print("{}: {}".format(count, arg[1]))
