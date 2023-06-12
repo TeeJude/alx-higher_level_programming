47e8cda770060e016f0b16633ce960f5c7c6dd42
Jude Tijani <teejayjude@gmail.com>
2023-06-12 10:48:36 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# function to replace an element in a list at a specific position without modifying the original list
+#function to replace an element in a list at a specific position without modifying the original list
 def new_in_list(my_list, idx, element):
     temp = my_list[:]
     if 0 <= idx < len(my_list):
