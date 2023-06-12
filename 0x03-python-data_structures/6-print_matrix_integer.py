4e2c69c907c2b0ea7933d98c0bb94cb5cff2060c
Jude Tijani <teejayjude@gmail.com>
2023-06-12 11:34:18 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#function to print a matrix of integers
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else"")
+        print()
