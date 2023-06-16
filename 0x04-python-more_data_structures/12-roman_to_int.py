14347b81568310da79d51529a2ffa978f757fc7b
Jude Tijani <teejayjude@gmail.com>
2023-06-16 11:00:16 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 500, 'M': 1000}
+    num = [rom[x] for x in roman_string] + [0]
+    r = 0
+    for i in range(len(num) - 1):
+        if num[i] >= num[i+1]:
+            r = r + num[i]
+        else:
+            r = r - num[i]
+    return r
