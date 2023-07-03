c74c69fb06bbc561abaeda203778b9820ec5c2c5
TeeJude <teejayjude@gmail.com>
2023-07-03 06:06:53 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
-""" Define a Rectangle class"""
+""" Define a Rectangle class."""
 
 class Rectangle:
-    """Depict a rectangle object"""
+    """Represent a rectangle."""
     pass
