67b6f0a8ebbb6562e081f81b8b0e2a0f58cbb33c
TeeJude <teejayjude@gmail.com>
2023-07-05 10:10:46 -0700

    8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,17 +85,17 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rec_diag = []
+        rec = []
         for i in range(self.__height):
-            [rec_diag.append(str(self.print_symbol)) for j in range(self.__width)]
+            [rec.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
-                rec_diag.append("\n")
-        return ("".join(rec_diag))
+                rec.append("\n")
+        return ("".join(rec))
 
     def __repr__(self):
         """Return string representation of rectangle to recreate instance."""
-        rec_diag = "Rectangle(" + str(self.__width)
-        rec_diag = rec_diag + ", " + str(self.__height) + ")"
+        rec = "Rectangle(" + str(self.__width)
+        rec = rec + ", " + str(self.__height) + ")"
         return (rec_diag)
 
     def __del__(self):
