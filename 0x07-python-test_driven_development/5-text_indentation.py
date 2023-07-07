573d442df6d5091a8a46682c2d2153829be24fc6
TeeJude <teejayjude@gmail.com>
2023-07-07 12:07:37 -0700

    5-text_indentation.py, tests/5-text_indentation.txt

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,7 +4,7 @@
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
+    """Print texts with two new lines after these characters '.', '?', and ':'.
 
     Args:
         text (string): The text to print.
