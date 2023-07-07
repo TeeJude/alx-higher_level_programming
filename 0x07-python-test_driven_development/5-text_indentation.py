b084ca5464b84904176affe8e3cc1ff4fa1ca520
TeeJude <teejayjude@gmail.com>
2023-07-07 11:53:46 -0700

    5-text_indentation.py, tests/5-text_indentation.txt

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -23,7 +23,7 @@ def text_indentation(text):
         if text[c] == "\n" or text[c] in ".?:":
             if text[c] in ".?:":
                 print("\n")
-            t += 1
+            c += 1
             while c < len(text) and text[c] == ' ':
                 c += 1
             continue
