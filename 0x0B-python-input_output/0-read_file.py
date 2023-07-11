e87bef84d46365365a7472bdc59652c755aaece5
TeeJude <teejayjude@gmail.com>
2023-07-11 14:08:04 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Prints the contents of a UTF8 text file to standard output."""
-    with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
