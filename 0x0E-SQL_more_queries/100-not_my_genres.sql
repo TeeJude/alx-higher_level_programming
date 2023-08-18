abc16b958b128935eb0f416eb677214c162ec1ec
TeeJude <teejayjude@gmail.com>
2023-08-17 17:01:03 -0700

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,4 +6,4 @@
 -- You can use a maximum of two SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title AS title FROM tv_shows WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = "Comedy") ORDER BY tv_shows.title ASC;
+SELECT DISTINCT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.genre_id NOT IN (SELECT tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = "Dexter") ORDER BY tv_genres.name ASC;
