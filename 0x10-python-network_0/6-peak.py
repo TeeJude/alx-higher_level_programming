f0d2dcc44d76667127a65146cc4376b524db84ce
TeeJude <teejayjude@gmail.com>
2023-09-28 09:42:04 -0700

    6-peak.py, 6-peak.txt

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+a function that finds a peak in a list of unsorted integers
+"""
+
+def find_peak(list_of_integers):
+    """
+    finds a peak in list_of_integers
+    """
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    low = 0
+    mid = ((high - low) // 2) + low
+    high = len(list_of_integers)
+    if high == 1:
+        return list_of_integers[0]
+    if high == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
