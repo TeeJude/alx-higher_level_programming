3dedbe78b76db16eb46e6fe154ff34d79f98cde2
TeeJude <teejayjude@gmail.com>
2023-09-29 11:39:40 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
+"""
+Script that takes in a URL, sends a request to the URL and displays 
+the body of the response.
 """
 import sys
 import requests
