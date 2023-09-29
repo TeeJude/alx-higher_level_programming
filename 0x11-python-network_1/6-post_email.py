bcffbb257d4bac1f84e16462e0447e2bd6ccb24e
TeeJude <teejayjude@gmail.com>
2023-09-29 11:46:19 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a POST request 
-to the passed URL with the email as a parameter, and display 
-the body of the response
+Script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter,
+- and display the body of the response
 """
 import sys
 import requests
